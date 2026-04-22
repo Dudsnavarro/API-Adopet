d3c2b14e8261b143616d25b7a143a2433b41e257
Dudsnavarro <eduardo.dudunavarro@oultook.com>
2026-04-22 08:43:39 -0300

    feat/pet interface

--- src/config/DataSource.ts
+++ src/config/DataSource.ts
@@ -1,8 +1,9 @@
 import { DataSource } from "typeorm";
+import PetEntity from "../intitys/PetEntitys";
 
 export const appDataSource = new DataSource({
     type:"sqlite",
     database:"./src/config/database.sqlite",
-    entities:[],
+    entities:[PetEntity],
     synchronize: true
 })
---
+++ src/intitys/PetEntitys.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export default class PetEntity {
+    @PrimaryGeneratedColumn()
+    id:number;
+    @Column()
+    nome:string;
+    @Column()
+    especie:string;
+    @Column()
+    dataDeNascimento:Date;
+    @Column()
+    adotado:boolean;
+}
